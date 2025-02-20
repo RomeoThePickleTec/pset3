b10e7f8cbc9864812574ee6908d876d9daac820b
Jesús Enrique Bañales López <A01642425@tec.mx>
2025-02-19 20:00:47 -0600

    Ahora si putines
    pene

--- SistemaVehicular.py
+++ SistemaVehicular.py
@@ -1,15 +1,20 @@
 class Vehicle:
     def transportar(self):
         print("Transporta...")
+    def numeroRuedas(self):
+        print("Por lo menos 1")
 
 class Car(Vehicle):
     def transportar(self):
         print("BRUMMMM.... BRUUUUURMMM...")
-
+    def numeroRuedas(self):
+        print("4")
 
 class Bicyle(Vehicle):
     def transportar(self):
         print("Voy a cruzar en rojo si me atropellan es culpa de ustedes")
+    def numeroRuedas(self):
+        print("2")
 
 class TransportSystem:
     def __init__(self, vehiculo:Vehicle):
@@ -17,3 +22,11 @@ class TransportSystem:
 
     def usarVehiculo(self):
         self.vehiculo.transportar()
+
+    def contarLlantas(self):
+        self.vehiculo.numeroRuedas()
+
+vehiculo = Car()
+JuanTrucks = TransportSystem(vehiculo)
+JuanTrucks.usarVehiculo()
+JuanTrucks.contarLlantas()
